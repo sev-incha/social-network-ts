ee5cbc3eee4b7756d79d7b679c9353c6f4c93d25
IlhomAliyev <aliyevilhom99@gmail.com>
2023-08-25 00:28:31 +0500

    updated favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from "react";
-import { ProfilePage } from "./pages/ProfilePage/ProfilePage";
 import { Header } from "./components/UI/Header/Header";
+import { LoginPage } from "./pages/LoginPage/LoginPage";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <Header />
       <div className="container">
-        <ProfilePage />
+        <LoginPage />
       </div>
     </div>
   );
