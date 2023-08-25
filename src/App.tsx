41b918d26f69b150a42f3993ad5d1c139fa7df15
TehnikumCodingFrontend <tehnikum.coding.frontend@gmail.com>
2023-08-25 10:57:09 +0500

    updated favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { Header } from "./components/UI/Header/Header";
 import { LoginPage } from "./pages/LoginPage/LoginPage";
+import { MainPage } from "./pages/MainPage/MainPage";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <Header />
       <div className="container">
-        <LoginPage />
+        {/* <LoginPage /> */}
+        <MainPage />
       </div>
     </div>
   );
